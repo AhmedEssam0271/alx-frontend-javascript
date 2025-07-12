44909c2c3add9187631f4aaf1ed6ebd657c02c55
AhmedEssam0271 <ahmedessam0271@gmail.com>
2025-07-12 18:15:42 +0300

    Extending the Teacher class
    Write an interface named Directors that extends Teacher. It requires an attribute named numberOfReports(number)

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,34 @@
 interface Teacher {
   readonly firstName: string; // only set at initialization
-  readonly lastName: string;  // only set at initialization
-  fullTimeEmployee: boolean;  // always required
+  readonly lastName: string; // only set at initialization
+  fullTimeEmployee: boolean; // always required
   yearsOfExperience?: number; // optional
-  location: string;           // always required
-  [key: string]: any;         // allow any extra properties
+  location: string; // always required
+  [key: string]: any; // allow any extra properties
 }
 
 const teacher3: Teacher = {
-  firstName: 'John',
+  firstName: "John",
   fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
+  lastName: "Doe",
+  location: "London",
   contract: false,
 };
 
 console.log(teacher3);
+
+// Define a new interface Directors that extends Teacher
+// and adds a numberOfReports property
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
